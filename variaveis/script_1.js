a5d60a852b0a7563de3bdd20f4cf6027e3ba8963
William <willunico10@hotmail.com>
2022-03-18 23:35:59 -0300

    Aulas JavaScript

---
+++ variaveis/script_1.js
@@ -0,0 +1,12 @@
+var nome = 'William'
+
+var numeroInteiro = 2
+
+var numeroFlutuante = 24.3
+
+var valorBooleano = true
+
+alert('O valor contido na variavel é: ' + nome + ' \nNumero de registro: ' + numeroInteiro)
+
+document.write('var nome: ' + nome)
+
